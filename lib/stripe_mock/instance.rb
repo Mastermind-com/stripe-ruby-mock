35fdc2ee712574235c3cccee80408d338c8229ca
Henrique Sarmento <henrique.sarmento@clickfunnels.com>
2020-07-22 07:54:58 -0300

    Add missing support for stripe v5.22

--- lib/stripe_mock/instance.rb
+++ lib/stripe_mock/instance.rb
@@ -52,11 +52,12 @@ module StripeMock
     include StripeMock::RequestHandlers::TaxRates
     include StripeMock::RequestHandlers::Checkout
     include StripeMock::RequestHandlers::Checkout::Session
+    include StripeMock::RequestHandlers::Prices
 
     attr_reader :accounts, :balance, :balance_transactions, :bank_tokens, :charges, :coupons, :customers,
                 :disputes, :events, :invoices, :invoice_items, :orders, :payment_intents, :payment_methods,
                 :setup_intents, :plans, :recipients, :refunds, :transfers, :payouts, :subscriptions, :country_spec,
-                :subscriptions_items, :products, :tax_rates, :checkout_sessions
+                :subscriptions_items, :products, :prices, :tax_rates, :checkout_sessions
 
     attr_accessor :error_queue, :debug, :conversion_rate, :account_balance
 
@@ -79,6 +80,7 @@ module StripeMock
       @orders = {}
       @payment_methods = {}
       @plans = {}
+      @prices = {}
       @products = {}
       @recipients = {}
       @refunds = {}
